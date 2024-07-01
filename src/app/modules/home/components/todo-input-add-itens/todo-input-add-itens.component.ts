ebce084ed44cc879da00b9cfdc78fd0fca419014
gajeel redfox <lucas.silva.cardoso1996@gmail.com>
2024-07-01 14:05:46 -0300

    adicioando função de deletar item por item da lista e deletar tudo

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
   styleUrls: ['./todo-input-add-itens.component.scss']
 })
-export class TodoInputAddItensComponent {
+export class TodoInputAddItensComponent implements OnInit{
+
+
+
+ngOnInit(): void {
+
+}
+
+pressEnter(event: KeyboardEvent){
+
+if(event.key == 'Enter'){
+
+  console.log('deu bom!');
+}
+
+
+}
 
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit{
+
+public taskList: Array<TaskList> = [
+  {task : "minhas primeira task", checked : true},
+  {task : "minhas primeira segunda", checked : false}
+
+];
+
+constructor(){}
+
+ngOnInit(): void {
+
+}
+
+public deleteItemTaskList(event: number){
+  this.taskList.splice(event,1);
+}
+
+public deleteAll(){
+
+  const confirm = window.confirm("Você deseja realmente deletar tudo?")
+  this.taskList = [];
+}
+
+
 
 }
